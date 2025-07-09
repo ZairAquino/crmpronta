0a5b2a78338eb783afab87d24f064404d46f5c74
Isa-hi <lacasademiamigochanel@gmail.com>
2025-07-08 19:50:10 -0600

    Eliminar funcionalidad de cambio de idiomas del sistema

--- frontend/src/pages/DashboardAppPage.js
+++ frontend/src/pages/DashboardAppPage.js
@@ -10,14 +10,8 @@ import Iconify from '../components/iconify';
 // sections
 import {
   AppTasks,
-  AppNewsUpdate,
   AppOrderTimeline,
-  AppCurrentVisits,
-  AppWebsiteVisits,
-  AppTrafficBySite,
   AppWidgetSummary,
-  AppCurrentSubject,
-  AppConversionRates,
 } from '../sections/@dashboard/app';
 import { apiget } from '../service/api';
 
@@ -99,108 +93,15 @@ export default function DashboardAppPage() {
             <AppWidgetSummary title="Tareas" total={totalEvent} color="error" icon={'mdi:events'} />
           </Grid>
 
-          <Grid item xs={12} md={6} lg={8}>
-            <AppWebsiteVisits
-              title="Visitas al Sitio Web"
-              subheader="(+43%) que el año pasado"
-              chartLabels={[
-                '01/01/2003',
-                '02/01/2003',
-                '03/01/2003',
-                '04/01/2003',
-                '05/01/2003',
-                '06/01/2003',
-                '07/01/2003',
-                '08/01/2003',
-                '09/01/2003',
-                '10/01/2003',
-                '11/01/2003',
-              ]}
-              chartData={[
-                {
-                  name: 'Equipo A',
-                  type: 'column',
-                  fill: 'solid',
-                  data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30],
-                },
-                {
-                  name: 'Equipo B',
-                  type: 'area',
-                  fill: 'gradient',
-                  data: [44, 55, 41, 67, 22, 43, 21, 41, 56, 27, 43],
-                },
-                {
-                  name: 'Equipo C',
-                  type: 'line',
-                  fill: 'solid',
-                  data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39],
-                },
-              ]}
-            />
-          </Grid>
 
-          <Grid item xs={12} md={6} lg={4}>
-            <AppCurrentVisits
-              title="Visitas Actuales"
-              chartData={[
-                { label: 'América', value: 4344 },
-                { label: 'Asia', value: 5435 },
-                { label: 'Europa', value: 1443 },
-                { label: 'África', value: 4443 },
-              ]}
-              chartColors={[
-                theme.palette.primary.main,
-                theme.palette.info.main,
-                theme.palette.warning.main,
-                theme.palette.error.main,
-              ]}
-            />
-          </Grid>
 
-          <Grid item xs={12} md={6} lg={8}>
-            <AppConversionRates
-              title="Tasas de Conversión"
-              subheader="(+43%) que el año pasado"
-              chartData={[
-                { label: 'Italia', value: 400 },
-                { label: 'Japón', value: 430 },
-                { label: 'China', value: 448 },
-                { label: 'Canadá', value: 470 },
-                { label: 'Francia', value: 540 },
-                { label: 'Alemania', value: 580 },
-                { label: 'Corea del Sur', value: 690 },
-                { label: 'Países Bajos', value: 1100 },
-                { label: 'Estados Unidos', value: 1200 },
-                { label: 'Reino Unido', value: 1380 },
-              ]}
-            />
-          </Grid>
 
-          <Grid item xs={12} md={6} lg={4}>
-            <AppCurrentSubject
-              title="Materia Actual"
-              chartLabels={['Inglés', 'Historia', 'Física', 'Geografía', 'Chino', 'Matemáticas']}
-              chartData={[
-                { name: 'Serie 1', data: [80, 50, 30, 40, 100, 20] },
-                { name: 'Serie 2', data: [20, 30, 40, 80, 20, 80] },
-                { name: 'Serie 3', data: [44, 76, 78, 13, 43, 10] },
-              ]}
-              chartColors={[...Array(6)].map(() => theme.palette.text.secondary)}
-            />
-          </Grid>
 
-          <Grid item xs={12} md={6} lg={8}>
-            <AppNewsUpdate
-              title="Actualización de Noticias"
-              list={[...Array(5)].map((_, index) => ({
-                id: faker.datatype.uuid(),
-                title: faker.name.jobTitle(),
-                description: faker.name.jobTitle(),
-                image: `/assets/images/covers/cover_${index + 1}.jpg`,
-                postedAt: faker.date.recent(),
-              }))}
-            />
-          </Grid>
+
+
+
+
+
 
           <Grid item xs={12} md={6} lg={4}>
             <AppOrderTimeline
@@ -220,33 +121,7 @@ export default function DashboardAppPage() {
             />
           </Grid>
 
-          <Grid item xs={12} md={6} lg={4}>
-            <AppTrafficBySite
-              title="Tráfico por Sitio"
-              list={[
-                {
-                  name: 'Facebook',
-                  value: 323234,
-                  icon: <Iconify icon={'eva:facebook-fill'} color="#1877F2" width={32} />,
-                },
-                {
-                  name: 'Google',
-                  value: 341212,
-                  icon: <Iconify icon={'eva:google-fill'} color="#DF3E30" width={32} />,
-                },
-                {
-                  name: 'Linkedin',
-                  value: 411213,
-                  icon: <Iconify icon={'eva:linkedin-fill'} color="#006097" width={32} />,
-                },
-                {
-                  name: 'Twitter',
-                  value: 443232,
-                  icon: <Iconify icon={'eva:twitter-fill'} color="#1C9CEA" width={32} />,
-                },
-              ]}
-            />
-          </Grid>
+
 
           <Grid item xs={12} md={6} lg={8}>
             <AppTasks
